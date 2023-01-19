beb6cb71b7b958da61d1a114607a8a5dfbe31c57
iwaken <iwakiri@craid-inc.com>
2023-01-19 19:45:01 +0700

    Add error log

--- run.py
+++ run.py
@@ -61,6 +61,7 @@ def show_datepicker(event, say: Say):
         query += f">{m}\n"
     say(text=f"{query}{result.text}", thread_ts=replies["messages"][0].get("thread_ts"))
 
+logging.basicConfig(level=logging.DEBUG)
 
 if __name__ == "__main__":
     handler = SocketModeHandler(app, SLACK_APP_TOKEN)
