e7252a79bf8a0d78e85021350fb372d15fe8e34b
iwaken <iwakiri@craid-inc.com>
2023-01-19 19:22:56 +0700

    Add git ignore

--- run.py
+++ run.py
@@ -5,9 +5,9 @@ from slack_bolt import Say
 from slack_bolt.adapter.socket_mode import SocketModeHandler
 import deepl
 
-SLACK_BOT_TOKEN = "xoxb-4051031086081-4051041571265-qH8YDWZOetmdkG1iBIUIOH1a"
-SLACK_APP_TOKEN = "xapp-1-A041H0YHNFK-4077265939473-c1b5f6da64228277f3bfd57e124407adf5913a7a8a662968e374f77cc266ae6e"
-DEEPL_API_TOKEN = "d857ebc3-1537-41ac-968f-e280bcb43793"
+SLACK_BOT_TOKEN = os.environ["SLACK_BOT_TOKEN"]
+SLACK_APP_TOKEN = os.environ["SLACK_APP_TOKEN"]
+DEEPL_API_TOKEN = os.environ["DEEPL_API_TOKEN"]
 
 app = App(token=SLACK_BOT_TOKEN)
 
